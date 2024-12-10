781aedc5cd52b461204a24e0025719ed086541fb
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-12-10 15:38:57 +0100

    add About page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // # Pages
 import HomePage from "./pages/HomePage";
+import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={HomePage} />
+        <Route path="/about" Component={About} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="container">
+      <h1>Chi Siamo</h1>
+    </div>
+  );
+}
